fdf79dcb20a5e1f184fd13d9d879920cebc08cb4
Jorne Lambrechts <r0625635@student.thomasmore.be>
2018-02-23 15:32:21 +0100

    wijzig welcome message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to APP Stef Goor</title>       
+	<title>Welcome to APP/BIT Stef Goor</title>       
 
 	<style type="text/css">
 
