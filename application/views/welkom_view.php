ae1c806d6cf2c46f7570960745c3f24f0ba00546
Joren Synaeve <jorensynaeve@hotmail.com>
2018-03-11 10:46:17 +0100

    Welkomstscherm uitproberen

--- application/views/welkom_view.php
+++ application/views/welkom_view.php
@@ -1,3 +1,5 @@
 <div class="page-header">
   <h1>Welkom</h1>
+  <p>Deze applicatie werd gerealiseerd om het organiseren van personeelsfeesten vlotter te laten verlopen.</p>
+  <p>De applicatie kan gebruikt worden door organisatoren, deelnemers en vrijwilligers. Je kan enkel toegang krijgen tot de applicatie via een uitnodigingsmail.</p>
 </div>
