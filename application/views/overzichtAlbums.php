2f263f35a0ef96d32c0b700a6c4f8d7abe3ab4f8
Stef Goor <r0663065@student.thomasmore.be>
2018-03-23 12:56:39 +0100

    overzicht albums 1

---
+++ application/views/overzichtAlbums.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file overzichtAlbums.php
+ * 
+ * View waarin een personeelslid/vrijwilliger het overzicht van de albums kan bekijken
+ */
+?>
+
+<div class="page-header">
+    <h1>Overzicht albums</h1>
+</div>
+
+<div>
+    <p>Op deze pagina vindt u het overzicht van fotoalbums van de vorige personeelsfeesten. Zo kan u alvast eens de sfeer opsnuiven.</p>
+</div>
+
+<div>
+    
+</div>
