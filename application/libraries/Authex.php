dcbd9ac24b214eeea4237c7838b27fdeb46ce359
Joren Synaeve <jorensynaeve@hotmail.com>
2018-03-23 10:26:05 +0100

    Authex toegevoegd aan libraries

---
+++ application/libraries/Authex.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Authex {
+
+    // +----------------------------------------------------------
+    // | TV Shop
+    // +----------------------------------------------------------
+    // | 2ITF - 201x-201x
+    // +----------------------------------------------------------
+    // | Authex library
+    // |
+    // +----------------------------------------------------------
+    // | Nelson Wells (http://nelsonwells.net/2010/05/creating-a-simple-extensible-codeigniter-authentication-library/)
+    // | 
+    // | aangepast door Thomas More
+    // +----------------------------------------------------------
+
+    public function __construct() {
+        $CI = & get_instance();
+
+        $CI->load->model('');
+    }
+}
