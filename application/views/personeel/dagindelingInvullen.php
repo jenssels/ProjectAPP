e266ae2d909cc23469ff10938050346606d7e365
Joren Synaeve <jorensynaeve@hotmail.com>
2018-03-28 12:41:05 +0200

    Joren

--- application/views/personeel/dagindelingInvullen.php
+++ application/views/personeel/dagindelingInvullen.php
@@ -11,13 +11,26 @@
     <p>De organisator heeft gekozen voor onderstaande dagindeling. Aan jou om de activiteiten te kiezen.</p>
 
     <?php
+    $attributenFormulier = array('id' => 'mijnFormulier',
+        'role' => 'form');
+    echo form_open('organisator/registreerNieuweOrganisator', $attributenFormulier)
+    ?>
+
+    <?php
     foreach ($dagindelingenMetOpties as $dagindeling) {
         echo "<h2>" . $dagindeling->naam . "</h2>";
         $options = array();
         foreach ($dagindeling->opties as $optie) {
             $options[] = $optie->naam;
         }
-        echo form_dropdown($dagindeling->id, $options);
+        $attributes = array('name' => '',
+            'id' => '',
+            'class' => 'form-control');
+        echo "<div class='form-group'>";
+        echo form_dropdown($dagindeling->id, $options, '', $attributes);
+        echo "</div>";
     }
     ?>
+    
+    <?php echo form_close(); ?>
 </div>
