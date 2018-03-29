388d4c605abb9ea46ad814cf603a13aeaf86b297
Joren Synaeve <jorensynaeve@hotmail.com>
2018-03-29 22:05:57 +0200

    Joren

--- application/views/organisator/beheerDagindeling.php
+++ application/views/organisator/beheerDagindeling.php
@@ -7,39 +7,72 @@
  * 
  */
 ?>
-
-<div class="page-header">
-    <h1><?php echo $titel; ?></h1>
+<div class="row">
+    <div class="page-header col-sm-12 pb-2">
+        <h1><?php echo $titel; ?> - <?php echo $personeelsfeest->naam ?></h1>
+    </div>
 </div>
 
-<div>
-    <p>Dit is de dagindeling van <?php echo $personeelsfeest->naam ?></p>
-    <table class="table">
-        <tr>
-            <th>Naam</th>
-            <th>Uur</th>
-            <th>Voor vrijwilliger</th>
-            <th></th>
-            <th></th>
-        </tr>
-        <?php
-        foreach ($dagindelingenBijFeest as $dagindeling) {
-            if ($dagindeling->voorVrijwilliger == 0) {
-                $voorVrijwilliger = 'Nee';
-            } else {
-                $voorVrijwilliger = 'Ja';
+<div class="row">
+    <div class="col-sm-6">
+        <table class="table">
+            <tr>
+                <th>Naam</th>
+                <th>Uur</th>
+                <th>Voor vrijwilliger</th>
+                <th></th>
+                <th></th>
+            </tr>
+            <?php
+            foreach ($dagindelingenBijFeest as $dagindeling) {
+                if ($dagindeling->voorVrijwilliger == 0) {
+                    $voorVrijwilliger = 'Nee';
+                } else {
+                    $voorVrijwilliger = 'Ja';
+                }
+
+                echo "<tr><td>" . $dagindeling->naam . "</td><td>" . $dagindeling->beginuur . " - " . $dagindeling->einduur .
+                "</td><td>" . $voorVrijwilliger . "</td><td>" . anchor('?', 'Bewerken') . "</td><td>" . anchor('organisator/verwijderDagindeling/' . $personeelsfeest->id . '/' . $dagindeling->id, 'Verwijderen') . "</td></tr>";
             }
-            
-            echo "<tr><td>" . $dagindeling->naam  . "</td><td>" . $dagindeling->beginuur . " - " .$dagindeling->einduur . 
-                    "</td><td>" . $voorVrijwilliger . "</td><td>" . anchor('?', 'Bewerken') . "</td><td>" . anchor('organisator/verwijderDagindeling/' .$personeelsfeest->id . '/' .$dagindeling->id, 'Verwijderen') . "</td></tr>";
-        }
-        ?>
-        <tr>
-            <td><?php echo anchor('organisator/toevoegenDagindeling/' . $personeelsfeest->id, 'Toevoegen') ?></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
+            ?>
+            <tr>
+                <td><?php echo anchor('organisator/toevoegenDagindeling/' . $personeelsfeest->id, 'Toevoegen') ?></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="col-sm-6">
+        <table class="table">
+            <tr>
+                <th>Naam</th>
+                <th>Uur</th>
+                <th>Voor vrijwilliger</th>
+                <th></th>
+                <th></th>
+            </tr>
+            <?php
+            foreach ($dagindelingenBijFeest as $dagindeling) {
+                if ($dagindeling->voorVrijwilliger == 0) {
+                    $voorVrijwilliger = 'Nee';
+                } else {
+                    $voorVrijwilliger = 'Ja';
+                }
+
+                echo "<tr><td>" . $dagindeling->naam . "</td><td>" . $dagindeling->beginuur . " - " . $dagindeling->einduur .
+                "</td><td>" . $voorVrijwilliger . "</td><td>" . anchor('?', 'Bewerken') . "</td><td>" . anchor('organisator/verwijderDagindeling/' . $personeelsfeest->id . '/' . $dagindeling->id, 'Verwijderen') . "</td></tr>";
+            }
+            ?>
+            <tr>
+                <td><?php echo anchor('organisator/toevoegenDagindeling/' . $personeelsfeest->id, 'Toevoegen') ?></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
 </div>
