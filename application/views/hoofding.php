b45c5ed23fb55eaf1d43629373527344e1842bf5
Joren Synaeve <jorensynaeve@hotmail.com>
2018-04-27 12:55:22 +0200

    Joren: link naar overzicht in hoofding

--- application/views/hoofding.php
+++ application/views/hoofding.php
@@ -3,14 +3,18 @@
         <h1><?php echo anchor('home/index', 'Personeelsfeest') ?></h1>
         <span class='navbar-text'>
             <?php
-        if ($this->session->userdata('emailgebruiker')) {
-            echo "<i class='fas fa-user'></i> " . $this->session->userdata('emailgebruiker');
-            echo " | ";
-            echo anchor('home/afmelden', '<i class="fas fa-sign-out-alt"></i>  Afmelden');
-        } else {
-            echo "<i class='fas fa-user'></i> " . anchor('home/aanmelden', 'Organisator login');
-        }
-        ?>
+            if ($this->session->userdata('emailgebruiker')) {
+                echo "<i class='fas fa-user'></i> " . $this->session->userdata('emailgebruiker');
+                echo "  |  ";
+                if ($this->session->userdata('gebruikerTypeId') == 1) {
+                    echo anchor('organisator/personeelsFeestOverzicht', '<i class="fas fa-list-ul"></i>  Overzicht');
+                    echo "  |  ";
+                }
+                echo anchor('home/afmelden', '<i class="fas fa-sign-out-alt"></i>  Afmelden');
+            } else {
+                echo "<i class='fas fa-user'></i> " . anchor('home/aanmelden', 'Organisator login');
+            }
+            ?>
         </span>        
     </div>
 </nav>
