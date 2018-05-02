499e28a46c9288f56f074bffa9d925ffffbea2d1
Thomas Vansprengel <iamthadjawi@gmail.com>
2018-05-02 15:09:13 +0200

    Front-end aan alle pagina's

--- application/views/locatieBewerken.php
+++ application/views/locatieBewerken.php
@@ -12,30 +12,30 @@
 
 	
     <?php
-    $attributes = array('name' => 'mijnFormulier');
+    $attributes = array('name' => 'mijnFormulier', 'class' => 'table');
     echo form_open('organisator/pasLocatieAan', $attributes);
     ?>
     <table>
         <tr>
-            <td><?php echo form_label('Naam:', 'naam'); ?></td>
-            <td><?php echo form_input(array('value' => $locatie->naam,'name' => 'naam', 'id' => 'naam', 'size' => '50')); ?></td>
+            <td><?php echo form_labelpro('Naam:', 'naam'); ?></td>
+            <td><?php echo form_input(array('class' => 'form-control','value' => $locatie->naam,'name' => 'naam', 'id' => 'naam', 'size' => '50')); ?></td>
         </tr>
         <tr>
-            <td><?php echo form_label('Adres:', 'adres'); ?></td>
-            <td><?php echo form_input(array('value' => $locatie->adres,'name' => 'adres', 'id' => 'adres', 'size' => '50')); ?></td>
+            <td><?php echo form_labelpro('Adres:', 'adres'); ?></td>
+            <td><?php echo form_input(array('class' => 'form-control','value' => $locatie->adres,'name' => 'adres', 'id' => 'adres', 'size' => '50')); ?></td>
         </tr>
                 <tr>
-            <td><?php echo form_label('Plaats:', 'plaats'); ?></td>
-            <td><?php echo form_input(array('value' => $locatie->plaats,'name' => 'plaats', 'id' => 'plaats', 'size' => '50')); ?></td>
+            <td><?php echo form_labelpro('Plaats:', 'plaats'); ?></td>
+            <td><?php echo form_input(array('class' => 'form-control','value' => $locatie->plaats,'name' => 'plaats', 'id' => 'plaats', 'size' => '50')); ?></td>
         <tr>
             <td><?php echo form_hidden('id', $locatie->id); ?></td></td>
-            <td><?php echo form_submit('knop', 'Submit'); ?></td>
+            <td><?php echo form_submit('knop', 'Submit',"class='btn btn-primary'"); ?>
+<?php echo "<a class='btn btn-primary' href=\"javascript:history.go(-1)\">Terug</a>"; ?></td>
         </tr>
     </table>
 
     <?php echo form_close(); ?>
 
-    <?php echo "<a href=\"javascript:history.go(-1)\">Terug</a>"; ?>
 
 </body>
 </html>
