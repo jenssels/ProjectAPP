a330c238e948ce2799a786dc58d0fb6cbf86847e
Stef Goor <r0663065@student.thomasmore.be>
2018-05-16 14:39:29 +0200

    stef ajax fix

--- application/views/organisator/ajax_optiesBijDagindeling.php
+++ application/views/organisator/ajax_optiesBijDagindeling.php
@@ -1,22 +1,86 @@
-<table class="table">
-    <?php
-    if ($opties == null) {
-        echo "niks";
-    } else {
-        echo "<tr>";
-        echo "<th>Naam</th>";
-        echo "<th>Beschrijving</th>";
-        echo "<th>Max. deelnemers</th>";
-        echo "<th>Min. deelnemers</th>";
-        echo "</tr>";
-        foreach ($opties as $optie) {
-            echo "<tr><td>" . $optie->naam . "</td><td>" . $optie->beschrijving . "</td><td>" . $optie->maxAantal . "</td><td>" . $optie->minAantal . "</td></tr>";
-        }
-    }
-    ?>
-</table>
+<?php
+// TODO bewerken van opties ?>
+<script>
+    $(document).ready(function () {
+        $('.voegOptieToeLink').click(function (e) {
+            e.preventDefault();
+        });
 
-<!-- Roep dialoogvenster op via de knop -->
-<button type="button" class="btn btn-primary" data-toggle="modal" id="knopVoegOptieToe" data-target="#exampleModal" data-id="<?php echo $dagindeling->id ?>">
-  Toevoegen
-</button>
+        <?php
+        $phpvar = base_url('index.php/organisator/verwijderOptie/');
+        echo "var _href = '{$phpvar}'; \n";
+        $feestIDvar = $dagindeling->personeelsfeestId;
+        echo "var feestID = '{$feestIDvar}'; \n";
+        ?>
+        $('.verwijderOptie').click(function (e) {
+            var id = $(this).data('id');
+            $('#verwijderen').attr('href', _href + id + '/' + feestID);
+            $('#modalBevestig1').modal('show');
+            e.preventDefault();
+        });
+
+        $('.voegOptieToeLink').click(function(){
+           $('#voegOptieToe').modal('show');
+        });
+    });
+</script>
+
+<div class="row">
+    <div class="page-header col-sm-12 pb-2">
+        <h5>Opties bij "<?php echo strtolower($dagindeling->naam); ?>"</h5>
+    </div>
+</div>
+<?php
+if (count($opties) > 0) {
+    echo "<table class='table'>";
+    echo "<tr>";
+    echo "<th>Naam</th><th>Beschrijving</th><th>Maximum aantal</th><th>Minimum aantal</th><th>Locatie</th><th></th>";
+    echo "</tr>";
+    foreach ($opties as $optie) {
+        if ($optie->maxAantal != null) {
+            $maxAantal = $optie->maxAantal;
+        } else {
+            $maxAantal = "N.v.t.";
+        }
+        if ($optie->minAantal != null) {
+            $minAantal = $optie->minAantal;
+        } else {
+            $minAantal = "N.v.t.";
+        }
+
+        echo "<tr>";
+        echo "<td>" . $optie->naam . "</td>";
+        echo "<td>" . $optie->beschrijving . "</td>";
+        echo "<td>" . $maxAantal . "</td>";
+        echo "<td>" . $minAantal . "</td>";
+        echo "<td>" . $optie->locatie->naam . "</td>";
+        echo "<td>" . anchor('#!', '<i class="far fa-trash-alt grow"></i>', 'class="verwijderOptie" data-id="' . $optie->id . '"') . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<p>Er zijn nog geen opties bij deze dagindeling.</p>";
+}
+?>
+
+<!-- Bevestigdialoogvenster -->
+<div class="modal fade" id="modalBevestig1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Wil je doorgaan?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Ben je zeker dat je deze optie wilt verwijderen?
+            </div>
+            <div class="modal-footer">
+                <a href="" class="btn btn-secondary" id="verwijderen">Doorgaan</a>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuleren</button>
+            </div>
+        </div>
+    </div>
+</div>
